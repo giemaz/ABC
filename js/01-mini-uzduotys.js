fe6ed0aa08d9970b5be39480f9add0cf33c59f67
giemaz <ghaleonlt@gmail.com>
2022-12-11 16:40:46 +0200

    First commit

---
+++ js/01-mini-uzduotys.js
@@ -0,0 +1,50 @@
+//  1.
+const one = 1;
+console.log(one);
+
+const two = -6;
+console.log(two);
+
+const three = 0;
+console.log(three);
+
+// 2.
+
+const stringA = 'hello';
+console.log(stringA);
+
+const stringB = 'world';
+console.log(stringB);
+
+let stringC = '!';
+console.log(stringC);
+
+//3.
+const array = [1, 2, 3, 4, 5];
+console.log(array);
+
+//4.
+const arrayA = ['hello', 'world', 'cat', 'dog', 'cow'];
+console.log(arrayA);
+
+const arrayB = ['one', 'two', 'three', 'four', 'five'];
+console.log(arrayB);
+
+const arrayC = ['apple', 'mango', 'pear', 'kale', 'nuts'];
+console.log(arrayC);
+
+// -------------------Variable operations-------------------
+
+// 1.
+const sum = one + two + three;
+console.log(sum);
+
+//2.
+const sentence = `${stringA} ${stringB} ${stringC}`;
+console.log(sentence);
+
+//3.
+
+//4.
+const megaArray = `${arrayA}, ${arrayB}, ${arrayC}`;
+console.log(megaArray);
